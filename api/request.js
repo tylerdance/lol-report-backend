e8e23c8e185cc72d99aac32b6a755fd58eddcb3b
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-02-10 09:43:52 -0500

    axios request with league-exp v4

--- api/request.js
+++ api/request.js
@@ -1,11 +1,11 @@
 require ('dotenv').config()
-const axios = require('axios')
+const Axios = require('axios')
 const express = require('express')
 const router = express.Router()
-const LOL_API_KEY=process.env.LOL_API_KEY
+const LOL_API_KEY = process.env.LOL_API_KEY
 
 router.get('/request', (req, res) => {
-    axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1&api_key=${LOL_API_KEY}`)
+    Axios.get(`https://na1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/CHALLENGER/I?page=1&api_key=${LOL_API_KEY}`)
     .then(response => { 
         res.json(response.data) 
     })
