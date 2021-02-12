5b33211f2eb4e63f5fe60acdf2aa09b322b211d4
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-02-12 12:48:54 -0500

    added grandmaster, master, and diamond routes

--- api/request.js
+++ api/request.js
@@ -14,4 +14,37 @@ router.get('/request', (req, res) => {
     })
 })
 
+// grandmaster tier
+router.get('/grandmaster', (req, res) => {
+    Axios.get(`https://na1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/GRANDMASTER/I?page=1&api_key=${LOL_API_KEY}`)
+    .then(response => { 
+        res.json(response.data) 
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+// master tier
+router.get('/master', (req, res) => {
+    Axios.get(`https://na1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/MASTER/I?page=1&api_key=${LOL_API_KEY}`)
+    .then(response => { 
+        res.json(response.data) 
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+// diamond tier
+router.get('/diamond', (req, res) => {
+    Axios.get(`https://na1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1&api_key=${LOL_API_KEY}`)
+    .then(response => { 
+        res.json(response.data) 
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
 module.exports = router;
